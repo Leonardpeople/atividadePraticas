1e9b08a0b5e8de0a92de32f3df0a6da8192e5187
Leonardpeople <lep@lep-PC>
2014-02-18 19:10:24 -0300

    atividade da urna

--- Atividade/Ativ1/urna/Urna.java
+++ Atividade/Ativ1/urna/Urna.java
@@ -1,10 +1,60 @@
 package urna;
-
+import javax.swing.JOption;
 public class Urna {
-
+		
+	private int Nulos;
+	private int Branco;
+	private int[] candidatos = new int[3];
+	private static boolean secao = false;
+	
 	public Urna() {
-		// TODO Auto-generated constructor stub
+		this.votosEmBranco = 0;
+		this.votosNulos = 0;
+		for(int i = 0; i < candidatos.length; i++){
+			candidatos[i] = 0;
+		}
+	}
+
+	public String votar(int voto){
+		if(secao == true){
+			if(voto == 0){
+				candidatos[0] ++;
+			}else if(voto == 1){
+				candidatos[1] ++;
+			}else if(voto == 2){
+				candidatos[2] ++;
+			}else{
+				votosNulos ++;
+			}
+			return "Voto Realizado com sucesso!";
+		}else{
+			return "A secao esta fechada!";
+		}
 	}
 	
+	public String votoEmBranco(){
+		if(secao == true){
+			votosEmBranco +1;
+			return "Voto realizado com sucesso!";
+		}else{
+			return "A secao esta fechada!";
+		}
+	}
 	
+	public void iniciarSecao(){
+		secao = true;
+	}
+	
+	public void encerrarSecao(){
+		secao = false;
+	}
+	
+	public void apurarVotacao(){
+		System.out.println("Candidato 1: " + candidatos[0]);
+		System.out.println("Candidato 2: " + candidatos[1]);
+		System.out.println("Candidato 3: " + candidatos[2]);
+		System.out.println("Votos em Branco: " + Branco);
+		System.out.println("Votos Nulos: " + Nulos);
+	}
+
 }
